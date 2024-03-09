60e4f3490d4649e8eb7533771ede8c4fe185d9c3
HikmeT <hekmt111@gmail.com>
2024-03-09 19:13:34 +0300

    React

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import {Component} from "react"
+
+class App extends Component{
+  render(){
+    return <h1>Hello World</h1>;
+  }
+}
+
+/* 
+function App(){
+  return <h1>Hello Hikmet</h1>
+} */
+
+export default App;
+
